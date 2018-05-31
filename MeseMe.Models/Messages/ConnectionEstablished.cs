d4f18e3381d5af04bbd542a54046100abfb3cd5b
Lucaci Andrei <lucaciandrei@outlook.com>
2018-05-31 18:07:14 +0300

    updated the wip

---
+++ MeseMe.Models/Messages/ConnectionEstablished.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MeseMe.Models.Messages
+{
+	public class ConnectionEstablished
+	{
+		public Guid Me { get; set; }
+		public IEnumerable<Guid> Others { get; set; }
+	}
+}
--- MeseMe.Server/Server.cs
+++ MeseMe.Server/Server.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Linq;
 using System.Net.Sockets;
 using System.Runtime.Remoting.Messaging;
+using System.Text;
 using System.Threading;
 using MeseMe.Handshake;
 using MeseMe.Models.Messages;
@@ -9,43 +11,47 @@ using MeseMe.Server.Engine;
 using MeseMe.Server.Engine.Clients;
 using MeseMe.Server.Engine.Communication;
 using MeseMe.ServerLogger;
+using Newtonsoft.Json;
 
 namespace MeseMe.Server
 {
 	public class Server
 	{
-		private TcpListener _tcpListener;
+		private TcpListener _handshakeListener;
+		private TcpListener _messageListener;
 		private Thread _tcpListenerThread;
-		private readonly ConcurrentBag<MeseClient> _connectedTcpClients = new ConcurrentBag<MeseClient>();
-		private volatile bool _listening;
+		private readonly ConcurrentBag<MeseClient> _connectedClients = new ConcurrentBag<MeseClient>();
+		private volatile bool _handshakeListening;
+		private volatile bool _messageListening;
 
 		public void Start()
 		{
-			_listening = true;
+			_handshakeListening = true;
+			_messageListening = true;
 			_tcpListenerThread = new Thread(() =>
 			{
-				new Thread(Listen).Start();
+				new Thread(Handshake).Start();
 				//new Thread(Converse).Start();
 			}) {IsBackground = true};
 			_tcpListenerThread.Start();
 		}
 
-		public void Listen()
+		public void Handshake()
 		{
 			try
 			{
-				_tcpListener = new TcpListener(HS.Ip.Any, HS.Ports.HanshakePort);
-				_tcpListener.Start();
+				_handshakeListener = new TcpListener(HS.Ip.Any, HS.Ports.HanshakePort);
+				_handshakeListener.Start();
 
 				Logger.Info("Server is listening...");
 
-				while (_listening)
+				while (_handshakeListening)
 				{
-					var client = _tcpListener.AcceptTcpClient();
+					var client = _handshakeListener.AcceptTcpClient();
 					new Thread(() =>
 					{
 						var meseClient = new MeseClient(client);
-						_connectedTcpClients.Add(meseClient);
+						_connectedClients.Add(meseClient);
 					}).Start();
 				}
 			}
@@ -55,9 +61,50 @@ namespace MeseMe.Server
 			}
 		}
 
+		public void ListenForMessages()
+		{
+			try
+			{
+				_messageListener = new TcpListener(HS.Ip.Any, HS.Ports.MessagingPort);
+				_messageListener.Start();
+
+				while (_messageListening)
+				{
+					var client = _messageListener.AcceptTcpClient();
+				}
+			}
+		}
+
 		public void StartConversation()
 		{
 			
+		}
+
+		public void CompleteHandshake(MeseClient meseClient)
+		{
+			try
+			{
+				using (var ns = meseClient.TcpClient.GetStream())
+				{
+					if (ns.CanWrite)
+					{
+						var connectionEstablished = new ConnectionEstablished
+						{
+							Me = meseClient.Id,
+							Others = _connectedClients.Where(i => i.Id != meseClient.Id).Select(i => i.Id)
+						};
+
+						var encodedPart = JsonConvert.SerializeObject(connectionEstablished);
+						var bytes = Encoding.UTF8.GetBytes(encodedPart);
+						ns.Write(bytes, 0, bytes.Length);
+						Logger.Info($"Handshake succedded with client {meseClient.Id}");
+					}
+				}
+			}
+			catch (SocketException sockedException)
+			{
+				Logger.Error($"Socked exception {sockedException}");
+			}
 		}
 
 		public Message Read(TcpClient client)
@@ -87,10 +134,13 @@ namespace MeseMe.Server
 
 		public void ShutDown()
 		{
-			_listening = false;
-			_tcpListener.Stop();
+			_handshakeListening = false;
+			_handshakeListener.Stop();
 
-			foreach (var connectedTcpClient in _connectedTcpClients)
+			_messageListening = false;
+			_messageListener.Stop();
+
+			foreach (var connectedTcpClient in _connectedClients)
 			{
 				connectedTcpClient.TcpClient.Dispose();
 			}
