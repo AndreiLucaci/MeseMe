47dc84518f06c6cf28a48c5d5662d841669f9436
Lucaci Andrei <lucaciandrei@outlook.com>
2018-05-31 11:19:48 +0300

    Added prism infrastructure to project

---
+++ MeseMe.WPF/Shell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MeseMe.WPF
+{
+	/// <summary>
+	/// Interaction logic for Shell.xaml
+	/// </summary>
+	public partial class Shell : Window
+	{
+		public Shell()
+		{
+			InitializeComponent();
+		}
+	}
+}
